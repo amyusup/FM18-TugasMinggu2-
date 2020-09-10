336f5fbae040fadb9997ee9a0ce4b8ed0879f2d8
amyusup <64889634+amyusup@users.noreply.github.com>
2020-09-10 10:08:47 +0700

    Update Tugas2.js

--- Tugas2.js
+++ Tugas2.js
@@ -15,15 +15,15 @@ if (
   console.log(`Rata-rata = ${hasil}`);
 
   //grade
-  if (hasil > 90 && hasil <= 100) {
+  if (hasil >= 90 && hasil <= 100) {
     grade = "A";
-  } else if (hasil > 80 && hasil <= 89) {
+  } else if (hasil >= 80 && hasil <= 89) {
     grade = "B";
-  } else if (hasil > 70 && hasil <= 79) {
+  } else if (hasil >= 70 && hasil <= 79) {
     grade = "C";
-  } else if (hasil > 60 && hasil <= 69) {
+  } else if (hasil >= 60 && hasil <= 69) {
     grade = "D";
-  } else if (hasil > 0 && hasil <= 59) {
+  } else if (hasil >= 0 && hasil <= 59) {
     grade = "E";
   } else {
     grade = "Angka yang dimasukkan tidak benar";
